99f2b20277f39858e635234ac10181e046192881
philipwagner1994 <s142025@student.dhbw-mannheim.de>
2016-11-20 20:35:27 +0100

    second commit

---
+++ Docker/Dockerjava/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# start Taktstrasse
+java -jar /TaktstrasseOpcServer-0.0.1-SNAPSHOT.jar  -o /temp -d 1000 -amqp tcp://activemq:61616 -kafka kafka:9092 -topic prodData --fastforward &
+
+sleep 40
+#start consumer
+java -jar /data_collector-0.0.1-SNAPSHOT-jar-with-dependencies.jar &
+#endless loop
+/usr/bin/tail -f /dev/null
